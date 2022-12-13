1f7bf4684e3afd338ad0ddfd61fc678489cedcfd
Karen Gasparyan <Karen.Gasparyan@ukg.com>
2022-12-13 20:33:19 +0400

    change homework 1

--- src/group1/homework/Homework1.java
+++ src/group1/homework/Homework1.java
@@ -4,6 +4,6 @@ public class Homework1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Homework");
+        System.out.println("Hello from Homework 1");
     }
 }
